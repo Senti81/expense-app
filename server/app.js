e46854c85e45f50090b6b884bd25e840c8ba1c61
Matthias Büttgenbach <matthias.buettgenbach@cologne-intelligence.de>
2020-07-03 14:40:25 +0200

    moved errorHandler to middleware folder

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const rolesRoute = require('./routes/roles');
 const usersRoute = require('./routes/users');
+const { notFound, errorHandler } = require('./middlewares/errorHandler');
 const app = express();
 
 app.use(logger('dev'));
@@ -13,20 +14,7 @@ app.use(cookieParser());
 app.use('/roles', rolesRoute);
 app.use('/users', usersRoute);
 
-// catch 404 and foward to error handler
-app.use(function (req, res, next) {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-        message: err.message,
-        error: req.app.get('env') === 'development' ? err : {}
-    })
-});
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
---
+++ server/middlewares/errorHandler.js
@@ -0,0 +1,19 @@
+
+function notFound (req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+};
+
+function errorHandler (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+        message: err.message,
+        error: req.app.get('env') === 'development' ? err : {}
+    })
+};
+
+module.exports ={
+    notFound,
+    errorHandler
+}
