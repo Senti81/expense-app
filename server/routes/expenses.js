8280061d43d7e610292178551e7d6695f07a3190
Matthias Büttgenbach <matthias.buettgenbach@cologne-intelligence.de>
2020-07-06 20:41:00 +0200

    using verify middleware in expense routes

--- server/routes/expenses.js
+++ server/routes/expenses.js
@@ -1,32 +1,44 @@
 const router = require('express').Router();
 const knex = require('../db/connection');
+const verify = require('../middlewares/verifyToken');
 
-router.get('/', async (req, res) => {
+router.get('/', verify, async (req, res) => {
     const allExpenses = await knex('expenses');
     res.json(allExpenses);
 });
 
-router.get('/:id', async (req, res, next) => {
+router.get('/:id', verify, async (req, res, next) => {
     const expenseById = await getById(req.params.id).first();
     expenseById ? res.json(expenseById) : next();
 });
 
-router.get('/users/:id', async (req, res, next) => {
+router.get('/users/:id', verify, async (req, res, next) => {
     const expensesByUser = await knex('expenses').where('user_id', req.params.id);
     expensesByUser.length > 0 ? res.json(expensesByUser) : next();
 });
 
-router.post('/', async (req, res) => {
-    const newId = await knex('expenses').insert(req.body);
+router.post('/', verify, async (req, res) => {
+    const newId = await knex('expenses').insert({
+        amount: req.body.amount,
+        user_id: req.user.id
+    });
     res.json(await getById(newId).first());
 });
 
-router.put('/:id', async (req, res, next) => {
+router.put('/:id', verify, async (req, res, next) => {
+    
+    // PUT is only allowed, if role is ADMIN
+    if (req.user.role_id === 2)
+        return res.sendStatus(403);
     const updatedExpense = await getById(req.params.id).update(req.body);
     updatedExpense === 1 ? res.json(await getById(req.params.id).first()) : next();
 });
 
-router.delete('/:id', async (req, res, next) => {
+router.delete('/:id', verify, async (req, res, next) => {
+
+    // PUT is only allowed, if role is ADMIN
+    if (req.user.role_id === 2)
+        return res.sendStatus(403);
     const deletedExpense = await getById(req.params.id).del();
     deletedExpense === 1 ? res.json(deletedExpense) : next();
 });
