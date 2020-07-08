b1690e3b89e1dd381593ff9f2722b880972bc55d
Matthias Büttgenbach <matthias.buettgenbach@cologne-intelligence.de>
2020-07-08 15:37:41 +0200

    removed AxiosTest component

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,21 +2,18 @@
   <div id="app">
     <Navbar/>
     <Login/>
-    <AxiosTest/>
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar'
 import Login from './components/Login'
-import AxiosTest from './components/AxiosTest'
 
 export default {
   name: 'App',
   components: {
     Navbar,
-    Login,
-    AxiosTest
+    Login    
   }
 }
 </script>
