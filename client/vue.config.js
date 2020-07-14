66da17b4da2fa88a922511490ed06dac745ae615
Matthias Büttgenbach <matthias.buettgenbach@cologne-intelligence.de>
2020-07-14 12:40:03 +0200

    fixed vue.config.js

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,13 +1,15 @@
+const path = require('path')
+
 module.exports = {
-  "outputDir": "D:\\dev\\expense-app\\server\\public",
-  "devServer": {
-    "proxy": {
-      "/api": {
-        "target": "http://localhost:3000/"
+  outputDir: path.resolve(__dirname, '../server/public'),
+  devServer: {
+    proxy: {
+      '/api': {
+        target: "http://localhost:3000/"
       }
     }
   },
-  "transpileDependencies": [
-    "vuetify"
+  transpileDependencies: [
+    'vuetify'
   ]
 }
