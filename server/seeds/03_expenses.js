c0bdda527995058731211a6acfc39611bc6287ba
Matthias Büttgenbach <matthias.buettgenbach@cologne-intelligence.de>
2020-07-15 15:16:27 +0200

    updated seed

--- server/seeds/03_expenses.js
+++ server/seeds/03_expenses.js
@@ -5,11 +5,13 @@ exports.seed = function(knex) {
       return knex('expenses').insert([
         {
           amount: 10.00,
-          user_id: 1
+          user_id: 1,
+          created_at: '2020-07-01 08:00:00'
         },
         {
           amount: 20.00,
-          user_id: 2
+          user_id: 2,
+          created_at: '2020-07-01 10:00:00'
         },
       ]);
     });
